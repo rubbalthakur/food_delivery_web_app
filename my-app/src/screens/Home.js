c9ced7dd26bd11177643cbb701e93d2fa643931c
rubbalthakur <127667490+rubbalthakur@users.noreply.github.com>
2024-09-08 00:59:22 +0530

    Fixed carousel image issue and added demo user option

--- my-app/src/screens/Home.js
+++ my-app/src/screens/Home.js
@@ -40,13 +40,13 @@ export default function Home() {
               </div>
             </div>
             <div className="carousel-item active">
-              <img src="https://source.unsplash.com/random/?burger" className="d-block w-100" style={{ filter: "brightness(30%)", maxHeight: "800px" }} alt="..." />
+              <img src="https://images.unsplash.com/photo-1460306855393-0410f61241c7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YnVyZ2VyfGVufDB8fDB8fHww" className="d-block w-100" style={{ filter: "brightness(30%)", maxHeight: "500px", objectFit: "cover"}} alt="..." />
             </div>
             <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/?pastry" className="d-block w-100" style={{ filter: "brightness(30%)", maxHeight: "800px" }} alt="..." />
+              <img src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8fDA%3D" className="d-block w-100" style={{ filter: "brightness(30%)", maxHeight: "500px", objectFit: "cover"}} alt="..." />
             </div>
             <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/?barbeque" className="d-block w-100" style={{ filter: "brightness(30%)", maxHeight: "800px" }} alt="..." />
+              <img src="https://plus.unsplash.com/premium_photo-1683619761468-b06992704398?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2VyfGVufDB8fDB8fHww" className="d-block w-100" style={{ filter: "brightness(30%)", maxHeight: "500px", objectFit: "cover"}} alt="..." />
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
